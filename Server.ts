19ae92532f014e618be0491a25e9318e668a9f60
Ramraghul <169035553+jssbackup9@users.noreply.github.com>
2024-11-23 21:14:12 +0530

    test

--- Server.ts
+++ Server.ts
@@ -98,7 +98,7 @@ app.get("/test", (req: Request, res: Response) => {
 app.use('/api/v1', Route);
 
 // Swagger Doc
-app.use('/', swaggerUi.serve, swaggerUi.setup(masterSwagger));
+app.use('/test', swaggerUi.serve, swaggerUi.setup(masterSwagger));
 
 // Start the server
 app.listen(advancedConfig.port, () => {
